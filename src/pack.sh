0c2190d0f353fb3f3c48e7719bb3e738c2f0b02e
Andrew Harvey <andrew@alantgeo.com.au>
2018-04-04 20:25:22 +1000

    fix pack

--- src/pack.sh
+++ src/pack.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-format="$1"
-
 pack() {
     f="$1"
     b=`basename "$f" .shp`
-    echo "$f -> $b"
-    case "$format" in
-        xz) tar -cJf "data/outputs/shp/${b}.tar.xz" "data/outputs/shp/${b}"*;;
-        *) zip --junk-paths "data/outputs/shp/${b}.zip" "data/outputs/shp/${b}"*;;
+    echo "$f -> $b ($FORMAT)"
+    case "$FORMAT" in
+        xz)
+            tar -cJf "data/outputs/shp/${b}.tar.xz" "data/outputs/shp/${b}"*
+            ;;
+        *)
+            zip --junk-paths "data/outputs/shp/${b}.zip" "data/outputs/shp/${b}"*
+            ;;
     esac
 }
 export -f pack
+export FORMAT="$1"
 ls -1 data/outputs/shp/*.shp | parallel pack {}
